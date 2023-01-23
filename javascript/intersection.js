b70200c6c65c1812ca9dbc34637520e83f4749eb
AlphaCupcake10 <alphacupcake@outlook.com>
2023-01-23 14:40:57 +0530

    Added Intersection Observer

---
+++ javascript/intersection.js
@@ -0,0 +1,16 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) =>{
+        console.log(entry)
+        if(entry.isIntersecting)
+        {
+            entry.target.classList.add('show');
+        }
+        else
+        {
+            // if(entry.target.classList.contains("repeat"))
+                entry.target.classList.remove('show');
+        }
+    });
+});
+const animatedElements = document.querySelectorAll(".animate");
+animatedElements.forEach((el) => observer.observe(el));
